27cae37bd4adf4c6ba8d2dae42d73d701286a0ec
Stefano Tenuta <stapps@outlook.it>
2018-06-03 18:48:08 +0200

    Updated branch information.

--- ParallelRoadTool/ModInfo.cs
+++ ParallelRoadTool/ModInfo.cs
@@ -8,7 +8,7 @@ namespace ParallelRoadTool
     public class ModInfo : IUserMod
     {
         public const string Version = "0.0.1";
-        public const string Branch = "master";
+        public const string Branch = "dev";
 
         public ModInfo()
         {
