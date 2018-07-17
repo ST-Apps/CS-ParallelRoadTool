e8651a7e07c55b88523df2d31fa5985255a05040
Stefano Tenuta <stapps@outlook.it>
2018-07-17 19:15:54 +0200

    Bumped version up.

--- ParallelRoadTool/ModInfo.cs
+++ ParallelRoadTool/ModInfo.cs
@@ -7,7 +7,7 @@ namespace ParallelRoadTool
 {
     public class ModInfo : IUserMod
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 #if DEBUG
         public const string Branch = "dev";
 #endif
