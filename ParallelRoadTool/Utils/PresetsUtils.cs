b1a60684cd00a7a5ca8a113cea7ddbfd834ece47
Stefano Tenuta <stapps@outlook.it>
2018-10-28 11:34:56 +0100

    Cleaned PresetsUtils + moved some strings into localization file.

--- ParallelRoadTool/Utils/PresetsUtils.cs
+++ ParallelRoadTool/Utils/PresetsUtils.cs
@@ -1,99 +1,112 @@
-﻿using ColossalFramework;
-using ColossalFramework.UI;
-using ParallelRoadTool.Detours;
-using ParallelRoadTool.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
+using ColossalFramework;
+using ColossalFramework.Globalization;
+using ColossalFramework.UI;
+using ParallelRoadTool.Detours;
+using ParallelRoadTool.Models;
 
 namespace ParallelRoadTool.Utils
 {
+    /// <summary>
+    ///     Utility class used to allow presets saving, loading and deleting.
+    /// </summary>
     public class PresetsUtils
     {
         public static bool Export(string filename)
         {
-            string path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
-
-            DebugUtils.Log($"Trying to save {filename} to {path}");
-
-            Directory.CreateDirectory(Configuration.AutoSaveFolderPath);
-            List<PresetNetItem> PresetItems = Singleton<ParallelRoadTool>.instance.SelectedRoadTypes
-                .Select(NetTypeItem => new PresetNetItem { HorizontalOffset = NetTypeItem.HorizontalOffset, IsReversed = NetTypeItem.IsReversed, NetName = NetTypeItem.NetInfo.name, VerticalOffset = NetTypeItem.VerticalOffset })
-                .ToList();
-            var xmlSerializer = new XmlSerializer(typeof(List<PresetNetItem>));
+            var path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
             try
             {
-                using (StreamWriter streamWriter = new StreamWriter(path))
+                Directory.CreateDirectory(Configuration.AutoSaveFolderPath);
+                var presetItems = Singleton<ParallelRoadTool>.instance.SelectedRoadTypes
+                    .Select(i => new PresetNetItem
+                    {
+                        HorizontalOffset = i.HorizontalOffset,
+                        IsReversed = i.IsReversed,
+                        NetName = i.NetInfo.name,
+                        VerticalOffset = i.VerticalOffset
+                    })
+                    .ToList();
+                var xmlSerializer = new XmlSerializer(typeof(List<PresetNetItem>));
+                using (var streamWriter = new StreamWriter(path))
                 {
-                    xmlSerializer.Serialize(streamWriter, PresetItems);
+                    xmlSerializer.Serialize(streamWriter, presetItems);
                 }
             }
             catch (Exception e)
             {
-                DebugUtils.Log("Couldn't export networks");
+                DebugUtils.Log($"Failed exporting {filename} to {path}");
                 DebugUtils.LogException(e);
-                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("Export failed", "The networks couldn't be exported to '" + path + "'\n\n" + e.Message, true);          
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel")
+                    .SetMessage(
+                        Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "ExportFailedTitle"),
+                        string.Format(Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "ExportFailedMessage"), path),
+                        true);
                 return false;
             }
+
             return true;
         }
 
         public static void Import(string filename)
         {
-            string path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
-            var PresetItems = new List<PresetNetItem>();
-            var xmlSerializer = new XmlSerializer(typeof(List<PresetNetItem>));
+            var path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
             try
             {
-                using (StreamReader streamReader = new StreamReader(path))
+                List<PresetNetItem> presetItems;
+                var xmlSerializer = new XmlSerializer(typeof(List<PresetNetItem>));
+                using (var streamReader = new StreamReader(path))
                 {
-                    PresetItems = (List<PresetNetItem>)xmlSerializer.Deserialize(streamReader);
+                    presetItems = (List<PresetNetItem>) xmlSerializer.Deserialize(streamReader);
                 }
-            }
-            catch (Exception e)
-            {
-                DebugUtils.Log("Couldn't import networks");
-                DebugUtils.LogException(e);
-                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("Import failed", "The networks couldn't be imported from '" + path + "'\n\n" + e.Message, true);
-            }
-            Singleton<ParallelRoadTool>.instance.ClearItems();
-            Singleton<ParallelRoadTool>.instance.SelectedRoadTypes.Clear();
-            foreach (PresetNetItem preset in PresetItems)
-            {
-                NetInfo netInfo;
-                netInfo = PrefabCollection<NetInfo>.FindLoaded(preset.NetName);
-                if (netInfo != null)
+
+                Singleton<ParallelRoadTool>.instance.ClearItems();
+                Singleton<ParallelRoadTool>.instance.SelectedRoadTypes.Clear();
+                foreach (var preset in presetItems)
                 {
-                    DebugUtils.Log("Adding network:" + netInfo.name);
-                    var item = new NetTypeItem(netInfo, preset.HorizontalOffset, preset.VerticalOffset, preset.IsReversed);
+                    var netInfo = PrefabCollection<NetInfo>.FindLoaded(preset.NetName);
+                    if (netInfo == null) continue;
+
+                    DebugUtils.Log($"Adding network {netInfo.name} from preset {filename}");
+                    var item = new NetTypeItem(netInfo, preset.HorizontalOffset, preset.VerticalOffset,
+                        preset.IsReversed);
                     Singleton<ParallelRoadTool>.instance.SelectedRoadTypes.Add(item);
                     Singleton<ParallelRoadTool>.instance.AddItem(item);
                     NetManagerDetour.NetworksCount = Singleton<ParallelRoadTool>.instance.SelectedRoadTypes.Count;
                 }
-                else
-                {
-                    //TODO: action for missing networks needed here 
-                }
+            }
+            catch (Exception e)
+            {
+                DebugUtils.Log($"Failed importing {filename} from {path}");
+                DebugUtils.LogException(e);
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel")
+                    .SetMessage(
+                        Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "ImportFailedTitle"),
+                        string.Format(Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "ImportFailedMessage"), path),
+                        true);
             }
         }
 
         public static void Delete(string filename)
         {
-            string path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
+            var path = Path.Combine(Configuration.AutoSaveFolderPath, filename + ".xml");
             try
             {
-                if (File.Exists(path))
-                {
-                    File.Delete(path);
-                }
+                if (File.Exists(path)) File.Delete(path);
             }
             catch (Exception e)
             {
-                DebugUtils.Log("Couldn't delete file");
+                DebugUtils.Log($"Failed deleting {filename} from {path}");
                 DebugUtils.LogException(e);
-                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("Delete failed", "Couldn't delete file '" + path + "'\n\n" + e.Message, true);
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel")
+                    .SetMessage(
+                        Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "DeleteFailedTitle"),
+                        string.Format(Locale.Get($"{Configuration.ResourcePrefix}TEXTS", "DeleteFailedMessage"), path),
+                        true);
             }
         }
     }
